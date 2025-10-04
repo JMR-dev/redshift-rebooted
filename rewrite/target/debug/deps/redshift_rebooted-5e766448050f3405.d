515d50dc320a7f0f20258d2b4c00bf5c87676076
Jason Ross <jason.ross841@gmail.com>
2025-10-04 13:40:08 -0500

    removing uncessary files and folders

--- rewrite/target/debug/deps/redshift_rebooted-5e766448050f3405.d
+++
@@ -1,5 +0,0 @@
-/home/jasonross/workspace/redshift-rebooted/rewrite/target/debug/deps/libredshift_rebooted-5e766448050f3405.rmeta: src/main.rs
-
-/home/jasonross/workspace/redshift-rebooted/rewrite/target/debug/deps/redshift_rebooted-5e766448050f3405.d: src/main.rs
-
-src/main.rs:
--- rewrite/target/debug/deps/redshift_rebooted-76c307c921ae1a85.d
+++
@@ -1,5 +0,0 @@
-/home/jasonross/workspace/redshift-rebooted/rewrite/target/debug/deps/redshift_rebooted-76c307c921ae1a85: src/main.rs
-
-/home/jasonross/workspace/redshift-rebooted/rewrite/target/debug/deps/redshift_rebooted-76c307c921ae1a85.d: src/main.rs
-
-src/main.rs:
